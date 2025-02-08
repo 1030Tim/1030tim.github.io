07788b58cdc343907f00abddc4cb7c508e7d0b8e
juhn_chen <126213720+1030Tim@users.noreply.github.com>
2025-02-08 23:02:03 +0800

    Site updated: 2025-02-08 23:02:03

--- js/inFootTime.js
+++ js/inFootTime.js
@@ -21,7 +21,7 @@
         if (String(snum).length === 1) {
             snum = "0" + snum;
         }
-        document.getElementById("timeDate").innerHTML = "本站安全运行&nbsp" + dnum + "&nbsp天";
+        document.getElementById("timeDate").innerHTML = "我們活了&nbsp" + dnum + "&nbsp天";
         document.getElementById("timeDate").style.color = "black"; // 设置颜色为黑色
         document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
         document.getElementById("times").style.color = "black"; // 设置颜色为黑色
@@ -64,7 +64,7 @@
         if (String(snum).length === 1) {
             snum = "0" + snum;
         }
-        document.getElementById("timeDate").innerHTML = "本站安全运行&nbsp" + ynum + "&nbsp年" + dnum + "&nbsp天";
+        document.getElementById("timeDate").innerHTML = "我們活了&nbsp" + ynum + "&nbsp年" + dnum + "&nbsp天";
         document.getElementById("timeDate").style.color = "black"; // 设置颜色为黑色
         document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
         document.getElementById("times").style.color = "black"; // 设置颜色为黑色
